b23262e9c468120d8a185ccda5eaf695e6e3095c
Harsheta <harshetasharma5@gmail.com>
2024-03-20 14:43:42 -0400

    Created the businesslogic folder and pages folder. Also made the classes, made required pages and added the enumns in their appropriate classes.

---
+++ Assignment3/BusinessLogic/ReservationRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.BusinessLogic
+{
+    enum RequestStatus
+    {
+        Accepted,
+        Rejected,
+        Pending
+    }
+    internal class ReservationRequest
+    {
+    }
+}
