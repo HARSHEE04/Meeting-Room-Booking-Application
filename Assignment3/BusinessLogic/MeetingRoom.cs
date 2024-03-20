6262ce0130e91f1f5594602ab718bbe67b7f3ed3
Harsheta <harshetasharma5@gmail.com>
2024-03-20 15:15:39 -0400

    finished properties with validation checks and construcor for the MeetingRoom class, still need to add methods

--- Assignment3/BusinessLogic/MeetingRoom.cs
+++ Assignment3/BusinessLogic/MeetingRoom.cs
@@ -14,8 +14,89 @@ namespace Assignment3.BusinessLogic
         Auditorium
     }
 
+    //include properties for all field variables
+    //inlcude validation checks in the setters of these properties
+    //add XML comments in the end
 
+
+    //Shows the possible meeting venues
     internal class MeetingRoom
     {
+        private string _roomNumber;
+        private int _seatingCapacity;
+        private RoomLayoutType _layoutType;
+        private string _roomImageFileName; //you basically pass the image of the room as a parameter in the constructor
+
+
+        #region Properties
+
+        //Room Number property
+
+        public string RoomNumber
+        {
+            get { return _roomNumber; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Room number is required");
+                }
+                _roomNumber = value;
+            }
+        }
+
+        //Seating Capacity property
+        public int SeatingCapacity
+        {
+            get { return _seatingCapacity; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception("The seating capacity needs to be greater than 0");
+                }
+                _seatingCapacity = value;
+            }
+        }
+
+        //Room Layout property
+        public RoomLayoutType LayoutType
+        {
+            get { return _layoutType; }
+            set { _layoutType = value; }
+        }
+
+        //RoomImageFileName property
+        public string RoomImageFileName
+        {
+            get { return _roomImageFileName; }
+            set
+            {
+                if(string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("The string for the image of the room is required");
+                }
+                _roomImageFileName = value;
+            }
+        }
+
+        //RoomTypeIcon, this is a computed property 
+        public string RoomTypeIcon
+        {
+            get { return $"{_layoutType}_icon.png"; }
+        }
+
+        #endregion
+
+        #region constructor
+        public MeetingRoom(string roomNumber, int seatingCapacity, RoomLayoutType layoutType, string roomImageFileName)
+        {
+            RoomNumber= roomNumber;
+            SeatingCapacity= seatingCapacity;
+            LayoutType= layoutType;
+            RoomImageFileName= roomImageFileName;
+        }
+
+        
     }
 }
