2e8b9b15c26c2bd68a595f50ef5efd830a4f154b
Harsheta <harshetasharma5@gmail.com>
2024-03-27 12:16:38 -0400

    fixed up the UI for the ViewRequest page a little and went back to scratch to figure out view requests page so I deleted the ViewModel because it was not neeeded since it's functionalities were present in ReservationRequestManager class already.

--- Assignment3/Pages/ViewRequestsPage.xaml.cs
+++ Assignment3/Pages/ViewRequestsPage.xaml.cs
@@ -7,20 +7,20 @@ public partial class ViewRequestsPage : ContentPage
     private ReservationRequestManager _requestManager;
     private MeetingRoom _selectedRoom;
 
+   
     public ViewRequestsPage(MeetingRoom selectedRoom, ReservationRequestManager requestsList)
     {
         InitializeComponent();
         _selectedRoom = selectedRoom;
         _requestManager = requestsList;
-        BindingContext = selectedRoom;
-        BindingContext = requestsList;
 
-       
+        BindingContext = selectedRoom;
+
+
         AllRequestListview.ItemsSource = requestsList.ReservationRequests;
     }
 
-    public MeetingRoom SelectedRoom => _selectedRoom;
-    public List<ReservationRequest> ReservationRequests => _requestManager.ReservationRequests;
+ 
 
     private void OnBackToRooms(object sender, EventArgs e)
     {
